f5c298d2c874b14043195458382cbfd8d0d2de3d
NamPE286 <nambuihung654@gmail.com>
2025-02-10 07:07:07 +0700

    stone

--- src/game/TileMap.hpp
+++ src/game/TileMap.hpp
@@ -4,21 +4,24 @@
 #include <SDL2/SDL.h>
 #include "../media/Image.hpp"
 
-enum Object {
+enum Tile {
 	FIELD,
-	COBBLESTONE
+	GRASS,
+	STONE
 };
 
 class TileMap {
 	struct Texture {
 		Image* field = nullptr;
+		Image* grass = nullptr;
+		Image* stone = nullptr;
 	} texture;
 	SDL_Renderer* renderer = nullptr;
 	SDL_Rect src = { 0, 0, 0, 0 }, dest = { 0, 0, 0, 0 };
-	int map[20][20] = {};
+	int map[15][20] = {};
 public:
 	TileMap(SDL_Renderer* renderer);
 	~TileMap();
-	void load(int arr[20][15]);
+	void load(int arr[15][20]);
 	void render();
 };

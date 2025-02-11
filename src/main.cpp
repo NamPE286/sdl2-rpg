da659b954d21d15626688c3098cec4198b58db9c
NamPE286 <nambuihung654@gmail.com>
2025-02-11 20:46:55 +0700

    error catching

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
-﻿#include <iostream>
+﻿#include <stdexcept>
+#include <string>
+#include <SDL2/SDL.h>
 #include "game/game.hpp"
 
 int SDL_main(int argc, char* argv[]) {
 	Game game;
 
-	game.init_game_loop();
+	try {
+		game.init_game_loop();
+	}
+	catch (std::runtime_error err) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "An error occured", err.what(), NULL);
+	}
 
 	return 0;
 }
